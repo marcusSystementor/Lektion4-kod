f5a7139fa44287ff7b5bd0fea8be49affadbf66e
marcusSystementor <144769052+marcusSystementor@users.noreply.github.com>
2023-09-26 13:35:22 +0200

    Update StudentMain.java
    Added comments in the code.

--- StudentMain.java
+++ StudentMain.java
@@ -1,15 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        // Skapar en ny instans av student
         Student student1 = new Student("Student1","Test" , 1995);
 
+        // skriver ut student1 första namn.
         System.out.println(student1.firstName);
 
+        // Skickar in födelseår i metod getAge() i student1
+        // lagrar det vi får i return av getAge() i student 1 i int age.
+        
        int age = student1.getAge(2023);
-       System.out.println(age);
+           // Skriver ut age.
+        System.out.println(age);
 
     }
+    // Samma metod som i Student men här i en statisk variant.
+    // Dvs den tillhör klassen, inte objektet som vi skapade från ritningen Student.
     public static int getAge(int birthYear ,int currentYear) {
         return currentYear - birthYear;
     }
