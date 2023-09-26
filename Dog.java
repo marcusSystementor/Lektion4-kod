eb8c71d2690a146bf817bdcd2d6420741fb5d50a
marcusSystementor <144769052+marcusSystementor@users.noreply.github.com>
2023-09-26 11:40:41 +0200

    Updated Dogs

--- Dog.java
+++ Dog.java
@@ -13,6 +13,12 @@ public class Dog {
 
     }
 
+    public Dog(String race, int amountOfLegs, boolean hasTail) {
+        this.race = race;
+        this.amountOfLegs = amountOfLegs;
+        this.hasTail = hasTail;
+    }
+
     public Dog(boolean isAlive) {
         this.isAlive = isAlive;
     }
@@ -21,6 +27,10 @@ public class Dog {
         return this.isAlive;
     }
 
+    //Ny metod, sätter om hunden är levande eller död
+    public void setIsAlive(boolean alive) {
+        this.isAlive = alive;
+    }
  
 }
 
--- DogMain.java
+++ DogMain.java
@@ -12,23 +12,29 @@ public class DogMain {
 
        Dog dog2 = new Dog("Bulldog", 3, true, false);
 
+       // Ber användaren mata in ifall hunden ska vara levande eller ej.
        boolean dogIsAlive = scanner.nextBoolean();
        
+       // Skapar ett hundobjekt och matar in boolean-vrödet i dog3s konstuktor.
        Dog dog3 = new Dog(dogIsAlive);
 
        System.out.println(dog3.isAlive);
 
+
+       // Skapar en ArrayList av Dog
         ArrayList<Dog> dogList = new ArrayList<>();
 
 
-
+         // Lägger till de objekt vi skapat av Dog.   
         dogList.add(dog1);
         dogList.add(dog2);
 
+        // Printar ut race för varje hund i array:en
         for (int i = 0; i < dogList.size(); i++) {
             System.out.println(dogList.get(i).race);
         }
 
+        // IF-sats för att kolla hurvida hunden lever eller inte, om true köp hunden.
         if (dog2.isAlive) {
             System.out.println("Buy");
         }
@@ -36,7 +42,16 @@ public class DogMain {
             System.out.println("Dont buy");
         }
 
+        // NYTT EXEMPEL
+        // Ett annat sätt att sätta om en hund är levande eller inte.
+        //Skapar en ny Dog utan isAlive i konstruktorn, vi sätter den parameter efteråt.
+        Dog dog4 = new Dog("Tax", 4, true);
 
+        boolean dogIsAliveNyttExempel = scanner.nextBoolean();
+        // Skapade en ny metod under Dog.
+        dog4.setIsAlive(dogIsAliveNyttExempel);
+
+        System.out.println("Is the dog alive? " +dog4.isAlive);
      
 
     }
